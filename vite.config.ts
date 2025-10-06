696a3cba4c3da969b18e64f7ee07f82c9da2f3f8
Farhan Tanvir <64136272+FarhanXTanvir@users.noreply.github.com>
2025-10-07 01:50:18 +0600

    vite config fixed for deploying

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: "/SAP1/", // GitHub repository name for GitHub Pages
 });
